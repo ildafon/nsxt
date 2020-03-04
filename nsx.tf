43b3e0a8e9504abe470cae0eeb7be8b2eab461db
vmware <vmware@kvm-01a.corp.local>
2020-03-03 19:59:05 -0800

    update nsx.tf

--- nsx.tf
+++ nsx.tf
@@ -9,14 +9,86 @@ provider "nsxt" {
   retry_on_status_codes = [429]
 }
 
-resource "nsxt_ip_set" "ip_set1" {
-  description  = "IP Set provisioned by Terraform"
-  display_name = "IP Set"
+data "nsxt_logical_tier0_router" "tier0" {
+  display_name = "Tier-0-gateway-01"
+}
+
+data "nsxt_edge_cluster" "edge_cluster" {
+    display_name = "edge-cluster-02"
+}
+
+data "nsxt_transport_zone" "tz" {
+    display_name = "TZ-Overlay"
+}
+
+resource "nsxt_logical_tier1_router" "tier1" {
+  description                 = "RTR1 provisioned by Terraform"
+  display_name                = "tf-t1-01"
+  failover_mode               = "PREEMPTIVE"
+  edge_cluster_id             = data.nsxt_edge_cluster.edge_cluster.id
+  enable_router_advertisement = true
+  advertise_connected_routes  = true
+  advertise_static_routes     = true
+  advertise_nat_routes        = true
 
   tag {
-    scope = "color"
-    tag   = "blue"
+    scope = "provisioned_by"
+    tag   = "terraform"
   }
+}
 
-  ip_addresses = ["1.1.1.1", "2.2.2.2"]
+resource "nsxt_logical_switch" "ls" {
+  admin_state       = "UP"
+  description       = "LS1 provisioned by Terraform"
+  display_name      = "tf-ls-01"
+  transport_zone_id = data.nsxt_transport_zone.tz.id
+  replication_mode  = "MTEP"
+
+  tag {
+    scope = "provisioned_by"
+    tag   = "terraform"
+  }
+}
+
+resource "nsxt_logical_port" "ls_port" {
+    admin_state = "UP"
+    logical_switch_id = nsxt_logical_switch.ls.id
+
+  tag {
+    scope = "provisioned_by"
+    tag   = "terraform"
+  }
+}
+
+resource "nsxt_logical_router_link_port_on_tier0" "link_port_tier0" {
+  description       = "TIER0_PORT1 provisioned by Terraform"
+  logical_router_id = data.nsxt_logical_tier0_router.tier0.id
+
+  tag {
+    scope = "provisioned_by"
+    tag   = "terraform"
+  }
+}
+
+resource "nsxt_logical_router_link_port_on_tier1" "link_port_tier1" {
+  description                   = "TIER1_PORT1 provisioned by Terraform"
+  logical_router_id             = nsxt_logical_tier1_router.tier1.id
+  linked_logical_router_port_id = nsxt_logical_router_link_port_on_tier0.link_port_tier0.id
+
+  tag {
+    scope = "provisioned_by"
+    tag   = "terraform"
+  }
+}
+
+resource "nsxt_logical_router_downlink_port" "downlink_port" {
+  description                   = "TIER1_PORT1 provisioned by Terraform"
+  logical_router_id             = nsxt_logical_tier1_router.tier1.id
+  linked_logical_switch_port_id = nsxt_logical_port.ls_port.id
+  ip_address = "192.168.205.1/24"
+
+  tag {
+    scope = "provisioned_by"
+    tag   = "terraform"
+  }
 }
