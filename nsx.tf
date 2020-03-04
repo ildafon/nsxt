b23f996494eab732f4fbf9d4591bbc3f7474049a
Administrator@CORP.LOCAL <Administrator@CORP.LOCAL>
2020-03-04 14:27:06 -0800

    add output

--- nsx.tf
+++ nsx.tf
@@ -15,6 +15,10 @@ output "t1_id" {
     value = nsxt_logical_tier1_router.tier1.id
 }
 
+output "ls_id" {
+    value = nsxt_logical_switch.ls.id
+}
+
 locals {
     provisioned_by_scope = "provisioned_by"
     provisioned_by = "terraform-yanjun"
